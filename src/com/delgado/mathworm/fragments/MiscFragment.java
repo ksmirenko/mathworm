d9dd8d2c5c0ccfb355150a3fc9d138749101c559
Cyrus Smith <cyrussmith1865@gmail.com>
2014-10-09 01:09:48 +0400

    Added layout (not finished); swiping works.

---
+++ src/com/delgado/mathworm/fragments/MiscFragment.java
@@ -0,0 +1,19 @@
+package com.delgado.mathworm.fragments;
+
+import com.delgado.mathworm.R;
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class MiscFragment extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+	    Bundle savedInstanceState) {
+	View v = inflater.inflate(R.layout.frag_misc, container, false);
+	return v;
+    }
+    
+    // TODO: implement button functions
+}
